ea72021a6d2d762c9cc816e54a9db7b72fac775f
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-25 21:34:42 -0300

    init project

---
+++ desafio-01/cielo-backend-challenge/src/main/java/arthur/saito/cielobackendchallenge/CieloBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package arthur.saito.cielobackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CieloBackendChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CieloBackendChallengeApplication.class, args);
+    }
+
+}
---
+++ desafio-01/cielo-backend-challenge/src/test/java/arthur/saito/cielobackendchallenge/CieloBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package arthur.saito.cielobackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CieloBackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
