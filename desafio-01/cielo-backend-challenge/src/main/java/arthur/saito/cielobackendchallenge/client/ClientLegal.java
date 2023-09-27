0743ed179d6971333df7cf686e02296cab39c576
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-27 20:41:34 -0300

    feat: implementing ClientLegal subclass

---
+++ desafio-01/cielo-backend-challenge/src/main/java/arthur/saito/cielobackendchallenge/client/ClientLegal.java
@@ -0,0 +1,28 @@
+package arthur.saito.cielobackendchallenge.client;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ClientLegal extends Client{
+    @Size(max = 14)
+    @Pattern(regexp = "^[0-9]{14}$")
+    private String cnpj;
+    @Size(max = 14)
+    private String razaoSocial;
+    @Size(max = 4)
+    private String mcc;
+    @Size(max = 11)
+    @Pattern(regexp = "^[0-9]{11}$")
+    private String cpf;
+}
