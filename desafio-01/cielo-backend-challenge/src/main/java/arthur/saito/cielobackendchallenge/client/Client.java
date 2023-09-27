a28030940ecc1f4e3021cfe76e1390600d0c3a63
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-27 20:37:30 -0300

    feat: adding validations with Bean and jakarta

--- desafio-01/cielo-backend-challenge/src/main/java/arthur/saito/cielobackendchallenge/client/Client.java
+++ desafio-01/cielo-backend-challenge/src/main/java/arthur/saito/cielobackendchallenge/client/Client.java
@@ -1,6 +1,9 @@
 package arthur.saito.cielobackendchallenge.client;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.UUID;
@@ -19,6 +22,8 @@ public class Client {
 
     @Column(unique = true)
     private UUID uuid;
+    @Size(max = 50)
     private String name;
+    @Pattern(regexp = "^[a-zA-Z0-9_\\-\\.]+@[a-zA-Z0-9_\\-\\.]+\\.[a-zA-Z]{2,5}$")
     private String email;
 }
