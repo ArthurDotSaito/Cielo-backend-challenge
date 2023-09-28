dce84aa3f6293cdc501e1e455a800a3493a90089
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-27 21:41:12 -0300

    feat: adding legalClient DTO

---
+++ desafio-01/cielo-backend-challenge/src/main/java/arthur/saito/cielobackendchallenge/client/legalClient/ClientLegalRequestDTO.java
@@ -0,0 +1,13 @@
+package arthur.saito.cielobackendchallenge.client.legalClient;
+
+import java.util.UUID;
+
+public class ClientLegalRequestDTO {
+    private UUID uuid;
+    private String cnpj;
+    private String mcc;
+    private String cpf;
+    private String name;
+    private String email;
+
+}
