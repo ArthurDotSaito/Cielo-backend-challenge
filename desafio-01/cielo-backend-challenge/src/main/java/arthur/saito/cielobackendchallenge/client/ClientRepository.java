59ad0d91f114baabb55ec1d9ffdaf437b8a93add
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-30 01:06:59 -0300

    feat: implementing JPA

--- desafio-01/cielo-backend-challenge/src/main/java/arthur/saito/cielobackendchallenge/client/ClientRepository.java
+++ desafio-01/cielo-backend-challenge/src/main/java/arthur/saito/cielobackendchallenge/client/ClientRepository.java
@@ -1,8 +1,11 @@
 package arthur.saito.cielobackendchallenge.client;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ClientRepository {
+import java.util.UUID;
 
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    boolean existsByUuid(UUID uuid);
 }
